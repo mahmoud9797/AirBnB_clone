7e326d024b82195259a255cede468050a336e65e
Mahmoud <elmahallawy6@gmail.com>
2024-03-06 05:27:34 +0200

    mahmoud

--- models/base_model.py
+++ models/base_model.py
@@ -16,17 +16,35 @@ class BaseModel:
         kwargs : attributt and its value k and v concept
         """
         time_f = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
-            for k and v in kwargs.items:
-                if k != '__class__' and (k == created_at or k == updated_at):
-                    v = datetime.strptime(v, time_f)
-                    setattr(self, k, v)
+        if kwargs != 0:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if (k == "created_at" or k == "updated_at"):
+                        v = datetime.strptime(v, time_f)
+                        setattr(self, k, v)
         else:
-            self.id = (uuid4())
+            self.id = str(uuid4())
             curr_date = datetime.now()
-            created_at = curr_date
-            updated_at = curr_date
+            self.created_at = curr_date
+            self.updated_at = curr_date
 
-        def save(self):
-            """ method used to update the the updated date of object """
-            updated_at = timedate.now()
+    def save(self):
+        """ method used to update the the updated date of object """
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        return user readable string for the name of calss and its unique id
+        and dictionary contains all attributes
+        """
+        form_at = "[{}] ({}) {}"
+        cl_name = self.__class__.__name__
+        return form_at.format(cl_name, self.id, self.__dict__)
+
+    def to_dict(self):
+        """ return dictionary representation of the object """
+        dic_t = {**self.__dict__}
+        dic_t["__class__"] = self.__class__.__name__
+        dic_t["created_at"] = self.created_at.isoformat()
+        dic_t["updated_at"] = self.updated_at.isoformat()
+        return dic_t
