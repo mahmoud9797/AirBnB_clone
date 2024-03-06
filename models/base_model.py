da1452d0c3825f6848d92ac6146c83319e086959
Mahmoud <elmahallawy6@gmail.com>
2024-03-06 06:42:38 +0200

    mahmoud

--- models/base_model.py
+++ models/base_model.py
@@ -6,22 +6,21 @@ from uuid import uuid4
 
 
 class BaseModel:
-    """ Base model class represent the parent class of HBNB project """
+    """ Base model class represents the parent class of the HBNB project """
 
     def __init__(self, *args, **kwargs):
-        """ constructor """
-
         """
-        args : unused
-        kwargs : attributt and its value k and v concept
+        constructor
+        args: unused
+        kwargs: attribute and its value k, v concept
         """
         time_f = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs != 0:
+        if kwargs:
             for k, v in kwargs.items():
                 if k != '__class__':
-                    if (k == "created_at" or k == "updated_at"):
+                    if (k == 'created_at' or k == 'updated_at'):
                         v = datetime.strptime(v, time_f)
-                        setattr(self, k, v)
+                    setattr(self, k, v)
         else:
             self.id = str(uuid4())
             curr_date = datetime.now()
@@ -29,13 +28,14 @@ class BaseModel:
             self.updated_at = curr_date
 
     def save(self):
-        """ method used to update the the updated date of object """
+        """ method used to update the updated date of the object """
         self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        return user readable string for the name of calss and its unique id
-        and dictionary contains all attributes
+        return the user-readable string for the name of the class
+        and its unique id
+        and a dictionary containing all attributes
         """
         form_at = "[{}] ({}) {}"
         cl_name = self.__class__.__name__
@@ -43,7 +43,7 @@ class BaseModel:
 
     def to_dict(self):
         """ return dictionary representation of the object """
-        dic_t = {**self.__dict__}
+        dic_t = dict(self.__dict__)
         dic_t["__class__"] = self.__class__.__name__
         dic_t["created_at"] = self.created_at.isoformat()
         dic_t["updated_at"] = self.updated_at.isoformat()
