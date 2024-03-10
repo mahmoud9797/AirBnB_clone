c3d3e85fc5a508ff46db574328d4267770512c08
Mahmoud <elmahallawy6@gmail.com>
2024-03-10 06:51:09 +0200

    mahmoud

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,12 +4,11 @@ deserializes JSON file to instances """
 import os.path
 from models.base_model import BaseModel
 import json
-from models.user import User
+from models.state import State
+from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-from models.state import State
-from models.city import City
 
 
 class FileStorage:
