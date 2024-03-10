af1bc4b97b96b2f5729bd9638991f689959a7958
Mahmoud <elmahallawy6@gmail.com>
2024-03-10 08:39:33 +0200

    mahmoud

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ module for Amenity """
-from models.base_model import Basemodel
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ module for the city """
-from models.base_model import Basemodel
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
