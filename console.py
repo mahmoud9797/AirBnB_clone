8e0e579a46ea18e7053f3dd8330e0839ced7b869
Mahmoud <elmahallawy6@gmail.com>
2024-03-10 21:20:42 +0200

    mahmoud

--- console.py
+++ console.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """ module for console """
-import cmd 
+import cmd
+import json
+from models import storage
+from models.place import Place
+from models.base_model import BaseModel
+from models.review import Review
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +27,29 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ do nothing if the input is empty line or enter """
         pass
+
+    __classes_dict = {
+            "BaseModel",
+            "User",
+            "City",
+            "Place",
+            "Review",
+            "State",
+            "Amenity"
+            }
+
+    def do_create(self, arg):
+        """ command to create new class instance """
+        if not arg:
+            print("** class name missing **")
+        args_l = arg.split()
+        clas_name = args_l[0].strip()
+        if clas_name not in HBNBCommand.__classes_dict:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(clas_name)()
+            print(new_obj.id)
+            storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
